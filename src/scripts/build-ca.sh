9da4dd528034ee148c16f075d9c66c1b468d85a1
Julian Pawlowski <julian@pawlowski.me>
2018-08-02 15:19:29 +0200

    add DH

--- src/scripts/build-ca.sh
+++ src/scripts/build-ca.sh
@@ -5,13 +5,16 @@ PKI_NAME="House Automation"
 cd "${SVC_HOME}"
 ./easyrsa --batch init-pki
 ./easyrsa --batch --req-cn="${PKI_NAME} Cerficiate Authority" --subca-len=1 build-ca nopass
+[ -s dh.pem ] && cp dh.pem data/ || ./easyrsa --batch gen-dh
+chmod 444 "data/ca.crt" "data/dh.pem"
 
 for SUBCA in $(ls ${SVC_HOME}/ | grep -E "^.*-ca$"); do
   cd "${SVC_HOME}/${SUBCA}"
   ln -sf ../easyrsa .
   ./easyrsa --batch init-pki
   ./easyrsa --batch --req-cn="${PKI_NAME} Sub CA: ${SUBCA}" --subca-len=0 build-ca nopass subca
-  #[ -s dh.pem ] && cp dh.pem data/ || ./easyrsa --batch gen-dh
+  [ -s dh.pem ] && cp dh.pem data/ || ./easyrsa --batch gen-dh
+  chmod 444 "data/dh.pem"
   
   cd "${SVC_HOME}"
   ./easyrsa --batch import-req "${SUBCA}/data/req/ca.req" "${SUBCA}"
