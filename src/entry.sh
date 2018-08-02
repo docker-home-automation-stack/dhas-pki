e61a67007d3e11acd154de965bae55716833d7f1
Julian Pawlowski <julian@pawlowski.me>
2018-08-02 12:43:06 +0200

    no message

--- src/entry.sh
+++ src/entry.sh
@@ -27,6 +27,7 @@ if [ "${CMD}" = 'start' ]; then
 
   if [ -z "$(find "${SVC_HOME}" -maxdepth 1 | tail -n +2)" ]; then
     cp -r /pki.tmpl/* "${SVC_HOME}/"
+    chown -R -h ${SVC_USER}:${SVC_GROUP} "${SVC_HOME}"
   fi
 
   if [ "$(id -u)" = '0' ]; then
