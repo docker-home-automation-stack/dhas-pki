e839f750e78cc7eea62ca74323b1a15cf94e2200
Julian Pawlowski <julian@pawlowski.me>
2018-08-12 15:30:59 +0200

    no message

--- src/scripts/sign-requests.sh
+++ src/scripts/sign-requests.sh
@@ -26,7 +26,7 @@ for TYPE in client code email server; do
 
         # sign request
         if [ "${RET_CODE}" = '0' ]; then
-          SAN=$(openssl req -in "${REQ}" -text -noout | grep -A 1 "Subject Alternative Name:" | tail -n +2 | sed -e "s/ //g")
+          SAN=$(./easyrsa show-req "${BASENAME}" | grep -A 1 "Subject Alternative Name:" | tail -n +2 | sed -e "s/ //g")
           [ ! "${SAN}" = "" ] && SAN="--subject-alt-name=\"${SAN}\""
           echo "[${TYPE}-${ALGO}-ca] Signing '${BASENAME}'"
           RET_TXT=$(./easyrsa --batch ${SAN} sign-req ${TYPE} "${BASENAME}" 2>&1)
