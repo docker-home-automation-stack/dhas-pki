00b57e15be885eea9203c1675ff08fc5bdbc7733
Julian Pawlowski <julian@pawlowski.me>
2018-08-30 18:16:46 +0200

    no message

--- src/scripts/build-ca.sh
+++ src/scripts/build-ca.sh
@@ -21,12 +21,12 @@ echo " openssl: $(type openssl), $(openssl version)"
 CURRHOME="${HOME}"
 CURRDIR=$(pwd)
 
-SVC_HOME=${SVC_HOME:-/pki}
-HOME="${SVC_HOME}"
-REQS="${SVC_HOME}/fifo"
+PKI_HOME=${PKI_HOME:-${SVC_HOME:-/pki}}
+HOME="${PKI_HOME}"
+REQS="${PKI_HOME}/fifo"
 umask 0077
 
-cd "${SVC_HOME}"
+cd "${PKI_HOME}"
 
 echo -e "[Build PKI] General initialization ..."
 cp -rfv /pki.tmpl/* ./
@@ -39,13 +39,13 @@ else
   ln -sfv /usr/share/easy-rsa/easyrsa .
 fi
 
-LIST="$(ls ${SVC_HOME}/ | grep -E "^.*-ca$" | grep root-)"
+LIST="$(ls ${PKI_HOME}/ | grep -E "^.*-ca$" | grep root-)"
 if [ "${LIST}" = '' ]; then
-  echo "ERROR: ${SVC_HOME} does not contain any initial Root CA data structure"
+  echo "ERROR: ${PKI_HOME} does not contain any initial Root CA data structure"
   exit 1
 fi
 
-LIST="${LIST} $(ls ${SVC_HOME}/ | grep -E "^.*-ca$" | grep -v root-)"
+LIST="${LIST} $(ls ${PKI_HOME}/ | grep -E "^.*-ca$" | grep -v root-)"
 
 for CA in ${LIST}; do
   type=$(echo "${CA}" | cut -d "-" -f 1)
@@ -58,7 +58,7 @@ for CA in ${LIST}; do
 
   # Initialize CA
   echo -e "[Build PKI: ${CA}] Initializing ..."
-  cd "${SVC_HOME}/${CA}"
+  cd "${PKI_HOME}/${CA}"
   ln -sfv ../easyrsa .
   ./easyrsa --batch init-pki
 
@@ -76,7 +76,7 @@ for CA in ${LIST}; do
 
   # Encrypt private key with password
   echo -e "[Build PKI: ${CA}] Protecting private key with password in data/private/ca.passwd ..."
-  [ -s "${SVC_HOME}/${type}-${algo}-ca.passwd" ] && mv -f "${SVC_HOME}/${type}-${algo}-ca.passwd" "data/private/ca.passwd" || pwgen -1sy 42 1 > "data/private/ca.passwd"
+  [ -s "${PKI_HOME}/${type}-${algo}-ca.passwd" ] && mv -f "${PKI_HOME}/${type}-${algo}-ca.passwd" "data/private/ca.passwd" || pwgen -1sy 42 1 > "data/private/ca.passwd"
   openssl ${algo_openssl} -out "data/private/ca.key" -aes256 -in "data/private/ca.nopasswd.key" -passout file:data/private/ca.passwd
 
   # Sub CA specific only
@@ -89,15 +89,15 @@ for CA in ${LIST}; do
 
     # Sign Sub CA with Root CA
     echo -e "[Build PKI: ${CA}] Generating signed CA certificate with Root ${ALGO} CA ..."
-    cd "${SVC_HOME}/root-${algo}-ca"
-    ./easyrsa --batch import-req "${SVC_HOME}/${CA}/data/reqs/ca.req" "${CA}"
+    cd "${PKI_HOME}/root-${algo}-ca"
+    ./easyrsa --batch import-req "${PKI_HOME}/${CA}/data/reqs/ca.req" "${CA}"
     ./easyrsa --batch sign-req ca "${CA}"
-    cp -fv "data/issued/${CA}.crt" "${SVC_HOME}/${CA}/data/ca.crt"
-    cat "data/issued/${CA}.crt" "data/ca.crt" > "${SVC_HOME}/${CA}/data/ca-chain.crt"
+    cp -fv "data/issued/${CA}.crt" "${PKI_HOME}/${CA}/data/ca.crt"
+    cat "data/issued/${CA}.crt" "data/ca.crt" > "${PKI_HOME}/${CA}/data/ca-chain.crt"
 
   fi
 
-  cd "${SVC_HOME}/${CA}"
+  cd "${PKI_HOME}/${CA}"
 
   # Create PKCS#12 public certificate + private key file variants
   echo -e "[Build PKI: ${CA}] Generating CA file in PKCS#12 format ..."
@@ -111,8 +111,8 @@ for CA in ${LIST}; do
     openssl crl2pkcs7 -out "data/ca-bundle.der.p7b" -nocrl -outform der -certfile "data/ca.crt"
     openssl crl2pkcs7 -out "data/ca-bundle.pem.p7c" -nocrl -outform pem -certfile "data/ca.crt"
   else
-    openssl crl2pkcs7 -out "data/ca-bundle.der.p7b" -nocrl -outform der -certfile "data/ca.crt" -certfile "${SVC_HOME}/root-${algo}-ca/data/ca.crt"
-    openssl crl2pkcs7 -out "data/ca-bundle.pem.p7c" -nocrl -outform pem -certfile "data/ca.crt" -certfile "${SVC_HOME}/root-${algo}-ca/data/ca.crt"
+    openssl crl2pkcs7 -out "data/ca-bundle.der.p7b" -nocrl -outform der -certfile "data/ca.crt" -certfile "${PKI_HOME}/root-${algo}-ca/data/ca.crt"
+    openssl crl2pkcs7 -out "data/ca-bundle.pem.p7c" -nocrl -outform pem -certfile "data/ca.crt" -certfile "${PKI_HOME}/root-${algo}-ca/data/ca.crt"
   fi
 
   # Create OCSP responder certificate
